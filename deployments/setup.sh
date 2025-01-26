90ed4543acab417f8f8c110c2d627fe0655cf5d6
isongjosiah <isongjosiah@gmail.com>
2025-01-26 05:46:21 +0100

    chore: add path update

--- deployments/setup.sh
+++ deployments/setup.sh
@@ -29,6 +29,13 @@ $NETWORK_SCRIPT up createChannel -ca
 # Step 3: Start the chaincode container
 cd $NETWORK_DIR
 ./network.sh deployCC -ccn proteomic -ccp ../../../blockchain/chaincode/proteomic -ccl go
+export PATH=${PWD}/../bin:$PATH
+export FABRIC_CFG_PATH=$PWD/../config/
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
 
 # Step 4: Run Microservices and IPFS with Docker Compose
 cd ../..
